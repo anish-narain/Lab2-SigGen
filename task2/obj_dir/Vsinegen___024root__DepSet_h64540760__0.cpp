6f6695bab0a58720ec32215bdf4782a1b2b090c1
anish-narain <69715492+anish-narain@users.noreply.github.com>
2022-10-28 12:14:02 +0100

    task 2

--- task2/obj_dir/Vsinegen___024root__DepSet_h64540760__0.cpp
+++ task2/obj_dir/Vsinegen___024root__DepSet_h64540760__0.cpp
@@ -14,15 +14,15 @@ VL_INLINE_OPT void Vsinegen___024root___sequent__TOP__0(Vsinegen___024root* vlSe
     CData/*7:0*/ __Vdly__sinegen__DOT__address;
     // Body
     __Vdly__sinegen__DOT__address = vlSelf->sinegen__DOT__address;
-    vlSelf->dout2 = vlSelf->sinegen__DOT__sineRom__DOT__rom_array
-        [vlSelf->sinegen__DOT__address];
     __Vdly__sinegen__DOT__address = ((IData)(vlSelf->rst)
                                       ? 0U : (0xffU 
                                               & ((IData)(vlSelf->en)
                                                   ? 
-                                                 ((IData)(vlSelf->sinegen__DOT__address) 
-                                                  + (IData)(vlSelf->incr))
+                                                 ((IData)(3U) 
+                                                  + (IData)(vlSelf->sinegen__DOT__address))
                                                   : (IData)(vlSelf->sinegen__DOT__address))));
+    vlSelf->dout2 = vlSelf->sinegen__DOT__sineRom__DOT__rom_array
+        [vlSelf->sinegen__DOT__address];
     vlSelf->dout1 = vlSelf->sinegen__DOT__sineRom__DOT__rom_array
         [(0xffU & ((IData)(vlSelf->sinegen__DOT__address) 
                    + (IData)(vlSelf->incr)))];
